7f9f5dda2b9736e4616bb8d49824c360eb300217
piyali1213 <piyalidas173@gmail.com>
2023-07-08 13:28:01 -0400

    added two files july8

---
+++ src/july8/Loop8.java
@@ -0,0 +1,18 @@
+package july8;
+
+public class Loop8 {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+
+        num1 = 2;
+        while (num1 <= 10) {
+            num2 = num1 * 2;
+            num1++;
+            System.out.println("result : " + num2);
+
+        }
+    }
+}
+
+
---
+++ src/july8/NutralNumber.java
@@ -0,0 +1,18 @@
+package july8;
+
+public class NutralNumber {
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+
+        num1 =0;
+        num2=1;
+        while (num1 <= 10) {
+            num2 = num1 + num2 ;
+            num1++;
+            System.out.println("result : " + num2);
+
+        }
+    }
+}
+
