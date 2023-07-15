425625531d59f290b804644d61302a7420a8a243
piyali1213 <piyalidas173@gmail.com>
2023-07-14 23:13:08 -0400

    added files

--- src/assignment/AlphabetVC.java
+++ src/assignment/AlphabetVC.java
@@ -1,18 +1,18 @@
 package assignment;
 import java.util.Scanner;
-public class Alphabet {
-    public static void main(String[] args) {
+public class AlphabetVC {
+    public static void main(String[] args)
+    {
 
         Scanner input = new Scanner(System.in);
-            System.out.println("Enter the alphabet :");
+            System.out.println("Enter the letter :");
             char letter = input.next().charAt(0);
             if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
                 System.out.println("the alphabet is vowel ");
             } else {
                 System.out.println("the alphabet is consonant ");
-
+            }
 
             }
         }
 
-    }
--- src/assignment/OddNaturalNumberSum.java
+++ src/assignment/OddNaturalNumberSum.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 public class OddNaturalNumberSum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print(" Enter the term : ");
-        int n = input.nextInt();
-
+        // System.out.print(" Enter the term : ");
+        // int n = input.nextInt();
+        int n = 1;
         int sum = 0;
-        while (n <= 5)
-
-        {
+        int num;
+        while (n <= 5) {
             System.out.println("enter the number : ");
-            int num = input.nextInt();
-              if (num%2!=0)
-              {
-                sum = sum + num;
-                System.out.println("Sum = " + sum);
-              }
+            num = input.nextInt();
+
             n++;
+            if (num % 2 != 0) {
+                sum = sum + num;
+                System.out.println("Sum of odd numbers = " + sum);
+            }
 
 
         }
 
     }
 }
+
